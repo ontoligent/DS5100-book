3272ba58ec05586e4d3a89a77a4967e7f36d0c12
ontoligent <ontoligent@gmail.com>
2024-09-03 14:38:04 -0400

    Finished M08

--- notebooks/M08_PythonTesting/demo/math_ops.py
+++ notebooks/M08_PythonTesting/demo/math_ops.py
@@ -1,15 +1,28 @@
-# math_operations.py
+# math_ops.py
 
-def add(a, b):
-    return a + b
+class MathOps:
+    
+    def add(a, b):
+        return a + b
 
-def subtract(a, b):
-    return a - b
+    def subtract(a, b):
+        return a - b
 
-def multiply(a, b):
-    return a * b
+    def multiply(a, b):
+        return a * b
 
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero")
-    return a / b
+    def divide(a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b
+
+if __name__ == '__main__':
+    print("This is the MathOps class. It's meant to be imported into another module.")
+    print("But since you called it directly, I'll show you how it works!")
+    
+    x = 5
+    y = 10
+    print("5 + 10 =", MathOps.add(x, y))
+    print("5 - 10 =", MathOps.subtract(x, y))
+    print("5 * 10 =", MathOps.multiply(x, y))
+    print("5 / 10 =", MathOps.divide(x, y))
--- notebooks/M08_PythonTesting/demo/student_test.py
+++ notebooks/M08_PythonTesting/demo/student_test.py
@@ -4,18 +4,20 @@ import unittest
 class EnrollInTestCase(unittest.TestCase): 
     
     def test_01_is_numCoursincremented_correctly(self):
-        # Create student instance, adding some courses
+        """
+        Test if enrollInCourse() method successfully increments the
+        num_courses attribute of the Student object 
+        """
+        
         student1 = Student('Katherine', ['DS 5100'])
         student1.enroll_in_course("CS 5050")
         student1.enroll_in_course("CS 5777")
-        print(student1.courses)
-        print(student1.num_courses)
+        print('Course names:', student1.courses)
+        print('Course count:', student1.num_courses)
         
-        # Test
         expected = 3
-        # unittest.TestCase brings in the assertEqual() method
         self.assertEqual(student1.num_courses, expected)
                 
         
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main()
